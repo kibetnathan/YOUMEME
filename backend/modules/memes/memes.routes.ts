e26d0c834edadff14f82d4263ef723c9efa4a952
kibetnathan <kibetnat@gmail.com>
2026-06-22 12:08:08 +0300

    chore(router): add upload function to router

--- backend/modules/memes/memes.routes.ts
+++ backend/modules/memes/memes.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getFeedHandler, getMemeHandler } from "./memes.controller";
-import { optionalAuth } from "../../middleware/auth";
+import {
+  getFeedHandler,
+  getMemeHandler,
+  uploadMemeHandler,
+} from "./memes.controller";
+import { optionalAuth, requireAuth } from "../../middleware/auth";
 
 const router = Router();
 
 router.get("/feed", optionalAuth, getFeedHandler);
 router.get("/:id", optionalAuth, getMemeHandler);
+router.get("/upload", requireAuth, uploadMemeHandler);
 
 export default router;
